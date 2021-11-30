f1cf216ff90fa9c034e7d81ed43f7103e2b03af3
Mangeshi <thisismangeshi@gmail.com>
2021-11-30 16:27:24 +0530

    added home_page content and image

--- src/Cpmponents/Home.jsx
+++ src/Cpmponents/Home.jsx
@@ -1,15 +1,24 @@
 import "./Home.css";
-import home_image from "../Images/home.png";
+import home_image from "../Images/home-banner.png";
 const Home = () => {
     return (
         <>
             <div className="container">
                 <div className="row">
-                    <div className="col-md-6">
-                        <h1>Hello</h1>
+                    <div className="col-md-7 home-content">
+                        <h2 className="intro">Hello! I'm Mangeshi</h2>
+                        <h1 className="main-title">Front-End Developer</h1>
+                        <h2 className="intro">
+                            I design and develop responsive, optimized and
+                            efficient websites.
+                        </h2>
                     </div>
-                    <div className="col-md-6">
-                        <img src={home_image} alt="banner-img"></img>
+                    <div className="col-md-5 home-banner-img">
+                        <img
+                            className="img-fluid"
+                            src={home_image}
+                            alt="banner-img"
+                        ></img>
                     </div>
                 </div>
             </div>
