9539c7e37c0ad2a422fdf1cd0ad20462e0772cda
Mangeshi <thisismangeshi@gmail.com>
2021-12-01 14:22:46 +0530

    added about page

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
+import About from './Cpmponents/About';
 import Home from './Cpmponents/Home';
+
 
 function App() {
   return (
     <div className="App">
      <Home/>
+     <About/>
     </div>
   );
 }
--- src/Cpmponents/Home.jsx
+++ src/Cpmponents/Home.jsx
@@ -1,27 +1,29 @@
 import "./Home.css";
-import home_image from "../Images/2.png";
+import home_image from "../Images/home.png";
 const Home = () => {
     return (
         <>
-            <div className="container">
-                <div className="row">
-                    <div className="col-md-7 home-content">
-                        <h2 className="intro">Hello! I'm Mangeshi</h2>
-                        <h1 className="main-title">Front-End Developer</h1>
-                        <h2 className="intro">
-                            I design and develop responsive, optimized and
-                            efficient websites.
-                        </h2>
-                        <div className="hire-btn">
-                            <button className="btn hire-me">Hire Me</button>
+            <div className="home-wrapper">
+                <div className="container-fluid">
+                    <div className="row home-cont">
+                        <div className="col-md-5 home-banner-img">
+                            <img
+                                className="img-fluid home-img"
+                                src={home_image}
+                                alt="banner-img"
+                            ></img>
                         </div>
-                    </div>
-                    <div className="col-md-5 home-banner-img">
-                        <img
-                            className="img-fluid"
-                            src={home_image}
-                            alt="banner-img"
-                        ></img>
+                        <div className="col-md-7 home-content">
+                            <h2 className="intro">Hello! I'm Mangeshi</h2>
+                            <h1 className="main-title">Front-End Developer</h1>
+                            <h2 className="intro">
+                                I design and develop responsive, optimized and
+                                efficient websites.
+                            </h2>
+                            <div className="hire-btn">
+                                <button className="btn hire-me">Hire Me</button>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
