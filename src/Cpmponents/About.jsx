1aec00d69f3fad3cca0ab6d7bfa2a08f09feee8c
Mangeshi <thisismangeshi@gmail.com>
2021-12-01 15:55:33 +0530

    added styling to about page

--- src/Cpmponents/About.jsx
+++ src/Cpmponents/About.jsx
@@ -4,34 +4,37 @@ import about_img from "../Images/about_shap_bg.png";
 const About = () => {
     return (
         <>
-        <div className="about-wrapper">
-
-            <div className=" container">
-                <div className="row">
-                    <div className="col-md-5 about-title">
-                        <img
-                            className="img-fluid about-img"
-                            src={about_img}
-                            alt=""
-                        />
-                        <h1 className="about-heading">
-                            About Me
-                        <div className="hori-line"></div>
-                        </h1>
-                    </div>
-                    <div className="col-md-7 about-info">
-                        <h4>
-                            I am a self-taught developer and I started my
-                            journey past 1 year. At the moment I am tailoring my
-                            skills on HTML, CSS, JavaScript, Bootstrap and
-                            React. This gives me a lot of joy when I see my
-                            progress, while I am learning and trying to take
-                            action on what I have learned.
-                        </h4>
+            <div className="about-wrapper">
+                <div className=" container">
+                    <div className="row">
+                        <div className="col-md-7 about-info">
+                            <h5>
+                                I am a self-taught developer and I started my
+                                journey past 1 year. At the moment I am
+                                tailoring my skills on HTML, CSS, JavaScript,
+                                Bootstrap and React. This gives me a lot of joy
+                                when I see my progress, while I am learning and
+                                trying to take action on what I have learned.
+                            </h5>
+                            <div className="btns">
+                                <button className="btn view-work">View Work</button>
+                                <button className="btn  download-cv">Download CV</button>
+                            </div>
+                        </div>
+                        <div className="col-md-5 about-title">
+                            <img
+                                className="img-fluid about-img"
+                                src={about_img}
+                                alt=""
+                            />
+                            <h1 className="about-heading">
+                                About Me
+                                <div className="hori-line"></div>
+                            </h1>
+                        </div>
                     </div>
                 </div>
             </div>
-        </div>
         </>
     );
 };
